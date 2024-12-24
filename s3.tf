948657efb643d32afec0cefbca2310ac4773e477
y2k19289 <51677857+y2k19289@users.noreply.github.com>
2024-12-24 14:40:32 +0530

    first commit to s3

--- s3.tf
+++ s3.tf
@@ -1,5 +1,11 @@
-# Create an S3 bucket
-resource "aws_s3_bucket" "my_bucket" {
+# S3 bucket for lab-1
+resource "aws_s3_bucket" "lab-1" {
   for_each = toset(var.bucket_name)
-  bucket   = each.value
+  bucket = each.value
+   tags = merge (
+    var.common_tag,
+    {
+      Name = each.value
+    }
+   )
 }
